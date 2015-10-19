68002fac3757c072449770f34f0660537162bc78
Sean Livingstone <seand.livingstone@gmail.com>
2015-10-19 09:57:39 +0100

    Initial Commit

---
+++ OptionPricer/OptionPricer/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OptionPricer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
