eea3544ee8030b90bfbc627d85ecb824f7002af5
Sean Livingstone <seand.livingstone@gmail.com>
2015-10-19 14:21:47 +0100

    Add test suite using NUnit

---
+++ OptionPricer/OptionPricer/Pricing/Pricer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OptionPricer.Pricing
+{
+    public class BSOptionPricer
+    {
+        enum OptionType { Put, Call };
+        public double Strike { get; set; }
+        public double Spot { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public double DiscountFactor { get; set; }
+        public double Volatility { get; set; }
+
+        public double Price()
+        {
+            return this.Spot - this.Strike;
+        }
+
+    }
+}
---
+++ OptionPricer/OptionPricerTests/PricingTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OptionPricer.Pricing;
+
+namespace OptionPricerTests
+{
+    [TestFixture]
+    public class PricingTests
+    {
+        [Test]
+        public void TestBlackScholesPrice_ATM()
+        {
+            BSOptionPricer Pricer = new BSOptionPricer();
+            Pricer.Strike = 10.0;
+            Pricer.Spot = 10.0;
+            Assert.AreEqual(0.0, Pricer.Price());
+        }
+
+        [Test]
+        public void TestBlackScholesPrice_ITM()
+        {
+            BSOptionPricer Pricer = new BSOptionPricer();
+            Pricer.Strike = 10.0;
+            Pricer.Spot = 15.0;
+            Assert.AreEqual(5.0, Pricer.Price());
+        }
+
+        [Test]
+        public void TestBlackScholesPrice_OTM()
+        {
+            BSOptionPricer Pricer = new BSOptionPricer();
+            Pricer.Strike = 15.0;
+            Pricer.Spot = 10.0;
+            Assert.AreEqual(0.0, Pricer.Price());
+        }
+    }
+}
